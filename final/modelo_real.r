19f6231e1965509dd62d741503171576a118d06d
pserey <serey.pedro@gmail.com>
2023-06-09 16:51:45 -0300

    add: final functionality to modelo_real
    detect training data vs. predicting data
    bet on only predicting data

--- final/modelo_real.r
+++ final/modelo_real.r
@@ -2,23 +2,32 @@ library(tidyverse)
 library(dplyr)
 
 # hardcoded regression model following the results of the simulation for the best
-# average hit rate calculated: 65.7%
+# average hit rate calculated: 56.2%
 
 # independent variables, model threshold and training dataset size
-# threshold: 2.7
-# variables: MFTAGt , MFTHGt , MFTAGm , OddO2.5
-# trainind dataset size: 40
+# threshold: 2.5
+# variables: MFTAGt , MFTHGt , MFTAGm
+# trainind dataset size: 140
 
-# recieves csv with 50+ games (being at least 40 completed) and 
+# recieves csv with 50+ games (being at least 40 completed) and
 # returns the list of games that have a chance to get more than 2.5 goals
+
 predict_games <- function(games_file) {
 
-  training_size <- 40
+  data_raw <- read.csv(games_file)
+  trainable_rows <- sum(!is.na(data_raw$FTHG))
+
+  training_size <- 140
+
+  if (trainable_rows < training_size) {
+    print("Warning: there is not sufficient game data for predicting accurately.")
+    training_size <- trainable_rows
+  }
 
   csv_data <- pre_process(games_file, 40)
 
-  variables <- c("MFTAGt", "MFTHGt", "MFTAGm", "OddO2.5")
-  bet_thresh <- 2.7
+  variables <- c("MFTAGt", "MFTHGt", "MFTAGm")
+  bet_thresh <- 2.5
 
   attach(csv_data)
 
@@ -45,19 +54,21 @@ predict_games <- function(games_file) {
     # acessa dados dos 10 jogos após limit_input para previsão
     new_data <- csv_data[(limit_input + 1):(limit_input + offset_training), variables]
 
-    # faz a previsão para os 10 jogos com os dados acessados
-    goals_avg <- predict(adjustment, newdata = new_data)
+    # faz a previsão para os 10 jogos com os dados acessados (se algum deles não tem os gols registrados)
+    if (any(is.na(csv_data$FTHG[(limit_input + 1):(limit_input + offset_training)]))) {
+      goals_avg <- predict(adjustment, newdata = new_data)
 
-    for (j in 1:offset_training) {
+      for (j in 1:offset_training) {
 
-      # if predicted, add to list
-      if (is.na(goals_avg[j])) {
-        # protection agains NaN in list
-      } else if (goals_avg[j] > bet_thresh) {
-        safe_games <- c(safe_games, paste("Game", csv_data$jogo[(limit_input + j)], " | ", csv_data$HomeTeam[(limit_input + j)], "vs.", csv_data$AwayTeam[(limit_input + j)]))
+        # if predicted, add to list
+        if (is.na(goals_avg[j])) {
+          # protection agains NaN in list
+        } else if ((goals_avg[j] > bet_thresh) && (is.na(csv_data$FTHG[limit_input + j]))) {
+          safe_games <- c(safe_games, paste("Game", csv_data$jogo[(limit_input + j)], " | ", csv_data$HomeTeam[(limit_input + j)], "vs.", csv_data$AwayTeam[(limit_input + j)]))
+        }
       }
-    }
 
+    }
   }
 
   detach(csv_data)
@@ -77,14 +88,12 @@ pre_process <- function(file_name, training_set) {
 
   # ---------------------------------------------------------------------
 
-  csv_data_calc <- csv_data[1:40, ]
-
   # adição de uma coluna de gols totais que soma FTHG + FTAG nos dados
-  csv_data_calc <- csv_data_calc %>% mutate(csv_data_calc, total_goals = FTHG + FTAG)
+  csv_data <- csv_data %>% mutate(csv_data, total_goals = ifelse(is.na(FTHG) | is.na(FTAG), NA, FTHG + FTAG))
 
   # gols tomados pela equipe de fora e de casa
-  csv_data_calc <- mutate(csv_data_calc, FTHGt = csv_data_calc$FTAG)
-  csv_data_calc <- mutate(csv_data_calc, FTAGt = csv_data_calc$FTHG)
+  csv_data <- csv_data %>% mutate(csv_data, FTHGt = ifelse(is.na(FTAG), NA, FTAG))
+  csv_data <- csv_data %>% mutate(csv_data, FTAGt = ifelse(is.na(FTHG), NA, FTHG))
 
   csv_data <- csv_data %>% group_by(AwayTeam) %>% mutate(MFTAGm = ifelse(jogo > training_set, NA, lag(cummean(FTAG), default = 0))) %>% ungroup()
   csv_data <- csv_data %>% group_by(HomeTeam) %>% mutate(MFTHGm = ifelse(jogo > training_set, NA, lag(cummean(FTHG), default = 0))) %>% ungroup()
@@ -98,4 +107,5 @@ pre_process <- function(file_name, training_set) {
   return(csv_data)
 }
 
-predict_games("data/D1.csv")
+file <- "data/predict_final_test.csv"
+predict_games(file)
